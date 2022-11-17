0cf3b67f77b3eefc113231f84682b24be3613aed
hyoshizaki <79033192+hyoshizaki@users.noreply.github.com>
2022-11-17 16:41:40 -0500

    Add files via upload

--- BeepTest.asm
+++ BeepTest.asm
@@ -4,14 +4,92 @@
 
 ORG 0
 
-	; Get the switch values
-	IN     Switches
-	; Send to the peripheral
-	OUT    Beep
-	; Delay for 1 second
-	CALL   Delay
-	; Do it again
-	JUMP   0
+; Get the switch values
+	IN Switches
+	JPOS SomeNote
+	
+	LOADI 0
+	STORE Temp
+	OUT Beep
+	CALL Delay
+	JUMP End
+
+SomeNote:
+	IN Switches
+	AND Bit0
+	JZERO NotC4
+	LOAD C4
+	STORE Temp
+	OUT Beep
+	CALL Delay
+	JUMP End
+NotC4:
+	IN Switches
+	AND Bit1
+	JZERO NotD4
+	LOAD D4
+	STORE Temp
+	OUT Beep
+	CALL Delay
+	JUMP End
+NotD4:
+	IN Switches
+	AND Bit2
+	JZERO NotE4
+	LOAD E4
+	STORE Temp
+	OUT Beep
+	CALL Delay
+	JUMP End
+NotE4:
+	IN Switches
+	AND Bit3
+	JZERO NotF4
+	LOAD F4
+	STORE Temp
+	OUT Beep
+	CALL Delay
+	JUMP End
+NotF4:
+	IN Switches
+	AND Bit4
+	JZERO NotG4
+	LOAD G4
+	STORE Temp
+	OUT Beep
+	CALL Delay
+	JUMP End
+NotG4:
+	IN Switches
+	AND Bit5
+	JZERO NotA4
+	LOAD A4
+	STORE Temp
+	OUT Beep
+	CALL Delay
+	JUMP End
+NotA4:
+	IN Switches
+	AND Bit6
+	JZERO 0
+	LOAD B4
+	STORE Temp
+	OUT Beep
+	CALL Delay
+	JUMP End
+
+End:
+	IN Beep
+	STORE Note
+	LOAD Temp
+	SUB Note
+	OUT Hex0
+	Jump 0
+
+
+	
+	
+
 	
 ; Subroutine to delay for 0.2 seconds.
 Delay:
@@ -28,4 +106,25 @@ LEDs:      EQU 001
 Timer:     EQU 002
 Hex0:      EQU 004
 Hex1:      EQU 005
+Temp:	   EQU 006
 Beep:      EQU &H40
+Note:      EQU 007
+
+
+C4:		   DW 65
+D4: 	   DW 73
+E4:		   DW 82
+F4:  	   DW 87
+G4:		   DW 98
+A4:		   DW 110
+B4:		   DW 123
+
+
+Bit0:      DW &B0000000001
+Bit1:      DW &B0000000010
+Bit2:      DW &B0000000100
+Bit3:      DW &B0000001000
+Bit4:      DW &B0000010000
+Bit5:      DW &B0000100000
+Bit6:      DW &B0001000000
+AbsoluteValue:	DW &B0111111111
--- TONE_GEN.vhd
+++ TONE_GEN.vhd
@@ -25,8 +25,8 @@ ENTITY TONE_GEN IS
 		CS         : IN  STD_LOGIC;
 		SAMPLE_CLK : IN  STD_LOGIC;
 		RESETN     : IN  STD_LOGIC;
-		L_DATA     : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-		R_DATA     : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+		L_DATA     : INOUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+		R_DATA     : INOUT STD_LOGIC_VECTOR(15 DOWNTO 0);
 		IO_WRITE	  : IN STD_LOGIC;
 		KEY2		  : IN STD_LOGIC; --Volume Down
 		KEY3		  : IN STD_LOGIC  --Volume Up
@@ -35,12 +35,22 @@ END TONE_GEN;
 
 ARCHITECTURE gen OF TONE_GEN IS 
 
+	TYPE VOL_STATE_TYPE IS (WantNoUp, WantOneUp, WantTwoUp, WantThreeUp,
+									WantFourUp, WantOneDown, WantTwoDown,
+									WantThreeDown, WantFourDown, NoUp, OneUp,
+									TwoUp, ThreeUp, FourUp, OneDown,
+									TwoDown, ThreeDown, FourDown);
+	SIGNAL vol_state : VOL_STATE_TYPE;
+	
+	TYPE INTERVAL_STATE_TYPE IS (NoSound, Playing, TimeUp);
+	SIGNAL interval_state : INTERVAL_STATE_TYPE;
+	SIGNAL previousTuningWord : STD_LOGIC_VECTOR(12 downto 0);
+
 	SIGNAL phase_register : STD_LOGIC_VECTOR(15 DOWNTO 0); --changed, but may not compile
 	SIGNAL tuning_word    : STD_LOGIC_VECTOR(12 DOWNTO 0); --changed to make it 7-bit tuning word
 	SIGNAL sounddata      : STD_LOGIC_VECTOR(7 DOWNTO 0);
 	SIGNAL KEY_PRESSED    : STD_LOGIC;
-	SIGNAL R_DATA_SHIFT   : STD_LOGIC_VECTOR(15 DOWNTO 0);
-	SIGNAL L_DATA_SHIFT   : STD_LOGIC_VECTOR(15 DOWNTO 0);
+	SIGNAL prev_RL_DATA   : STD_LOGIC_VECTOR(15 downto 0);
 	SIGNAL counter 		 : STD_LOGIC_VECTOR(31 DOWNTO 0);
 	
 BEGIN
@@ -76,44 +86,12 @@ BEGIN
 						'0';
 	
 
-			
-			
---R_DATA <= R_DATA_SHIFT WHEN (NOT(R_DATA_SHIFT = "ZZZZZZZZZZZZZZZZ")) ELSE 
---			sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata&"0000";
---L_DATA <= L_DATA_SHIFT WHEN NOT(L_DATA_SHIFT = "ZZZZZZZZZZZZZZZZ") ELSE 
---			sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata&"0000";
-			
+
 	-- 8-bit sound data is used as bits 12-5 of the 16-bit output.
 	-- This is to prevent the output from being too loud.
 	-- for volume control, shift left/right?
 	-- for splitting, make a internal signal
-	L_DATA(15 DOWNTO 12) <= sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7); -- sign extend
-	L_DATA(11 DOWNTO 4) <= sounddata;
-	L_DATA(3 DOWNTO 0) <= "0000"; -- pad right side with 0s
-	
-	-- Right channel is the same.
-	R_DATA(15 DOWNTO 12) <= sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7); -- sign extend
-	R_DATA(11 DOWNTO 4) <= sounddata;
-	R_DATA(3 DOWNTO 0) <= "0000"; -- pad right side with 0s
-	
-
---	PROCESS(KEY2,KEY3, KEY_PRESSED) BEGIN
---		if (KEY3 = '1') AND (KEY2 = '0') AND NOT(R_DATA_SHIFT = (sounddata(7 downto 0) & "00000000")) THEN 
---			R_DATA_SHIFT <= R_data_SHIFT(14 downto 0) & '0';
---			L_DATA_SHIFT <= L_data_SHIFT(14 downto 0) & '0';
---		ELSIF (kEY2 = '1') AND (KEY3 = '0')  AND NOT(R_DATA_SHIFT = (sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7) & sounddata)) THEN
---			R_DATA_SHIFT <= sounddata(7) & R_DATA_SHIFT(15 downto 1);
---			L_DATA_SHIFT <= sounddata(7) & L_DATA_SHIFT(15 downto 1);
---		ELSIF (((kEY2 = '0') and (KEY3='0') AND (KEY_PRESSED = '1'))  OR ((KEY2 = '1') AND (KEY3 = '1'))) THEN --none of the buttons have been pressed or both volume control pressed
---			R_DATA_SHIFT <= R_DATA_SHIFT;
---			L_DATA_SHIFT <= L_DATA_SHIFT;
---		ELSE 	--first booted up
---			R_DATA_SHIFT <= "ZZZZZZZZZZZZZZZZ";
---			L_DATA_SHIFT <= "ZZZZZZZZZZZZZZZZ";
---		END IF;
---	END PROCESS;
- 	
-	-- process to perform DDS
+		-- process to perform DDS
 	PROCESS(RESETN, SAMPLE_CLK) BEGIN
 		IF RESETN = '0' THEN
 			phase_register <= "0000000000000000";
@@ -135,12 +113,84 @@ BEGIN
 	PROCESS(RESETN, CS) BEGIN
 		IF RESETN = '0' THEN
 			tuning_word <= "0000000000000";
+			previousTuningWord <= tuning_word;
 		ELSIF RISING_EDGE(CS) THEN
 			IF IO_WRITE = '1' THEN
 				tuning_word <= CONV_STD_LOGIC_VECTOR(CONV_INTEGER(CONV_INTEGER(CMD)*65536/48000), 13);
 			END IF;
 		END IF;
 	END PROCESS;
+	
+	--PROCESS statement to control volume
+	PROCESS(KEY2, KEY3, resetn, SAMPLE_CLK) BEGIN
+		if(resetn = '0') then
+			vol_state <= NoUp;
+		elsif(RISING_EDGE(SAMPLE_CLK)) THEN
+			IF ((KEY3 = '0') AND (KEY2 = '1')) THEN --KEY3 PRESSED and released so volume up
+				CASE vol_state IS
+					WHEN FourDown => vol_state <= WantThreeDown;
+					WHEN ThreeDown => vol_state <= WantTwoDown;
+					WHEN TwoDown => vol_state <= WantOneDown;
+					WHEN OneDown => vol_state <= WantNoUp;
+					WHEN NoUp => vol_state <= WantOneUp;
+					WHEN OneUp => vol_state <= WantTwoUp;
+					WHEN TwoUp => vol_state <= WantThreeUp;
+					WHEN ThreeUp => vol_state <= WantFourUp;
+					WHEN OTHERS => vol_state <= vol_state;
+				END Case;
+				prev_RL_DATA <= R_DATA;
+			elsif ((KEY2 = '0') AND (KEY3 = '1')) THEN --KEY2 pressed and released so volume down
+				CASE vol_state is
+					WHEN FourUp => vol_state <= WantThreeUp;
+					WHEN ThreeUp => vol_state <= WantTwoUp;
+					WHEN TwoUp => vol_state <= WantOneUp;
+					WHEN OneUp => vol_state <= WantNoUp;
+					WHEN NoUp => vol_state <= WantOneDown;
+					WHEN OneDown => vol_state <= WantTwoDown;
+					WHEN TwoDown => vol_state <= WantThreeDown;
+					WHEN ThreeDown => vol_state <= WantFourDown;
+					WHEN OTHERS => vol_state <= vol_state;
+				END CASE;
+				prev_RL_DATA <= R_DATA;
+			elsif ((KEY2 = '1') AND (KEY3 = '1')) THEN
+				CASE vol_state is
+					WHEN WantFourDown => vol_state <= FourDown;
+					WHEN WantThreeDown => vol_state <= ThreeDown;
+					WHEN WantTwoDown => vol_state <= TwoDown;
+					WHEN WantOneDown => vol_state <= OneDown;
+					WHEN WantNoUp => vol_state <= NoUp;
+					WHEN WantOneUp => vol_state <= OneUp;
+					WHEN WantTwoUp => vol_state <= TwoUp;
+					WHEN WantThreeUp => vol_state <= ThreeUp;
+					WHEN WantFourUp => vol_state <= FourUp;
+					WHEN OTHERS => vol_state <= vol_state;
+				END CASE;
+			END IF;
+		END IF;
+	end process;
+ 	
+	WITH vol_state SELECT R_DATA <=
+		sounddata&"00000000" WHEN FourUp,
+		sounddata(7)&sounddata&"0000000" WHEN ThreeUp,
+		sounddata(7)&sounddata(7)&sounddata&"000000" WHEN TwoUp,
+		sounddata(7)&sounddata(7)&sounddata(7)&sounddata&"00000" WHEN OneUp,
+		sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata&"0000" WHEN NoUp,
+		sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata&"000" WHEN OneDown,
+		sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata&"00" WHEN TwoDown,
+		sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata&"0" WHEN ThreeDown,
+		sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata WHEN FourDown,
+		prev_RL_DATA WHEN OTHERS;
+	WITH vol_state SELECT L_DATA <=
+		sounddata&"00000000" WHEN FourUp,
+		sounddata(7)&sounddata&"0000000" WHEN ThreeUp,
+		sounddata(7)&sounddata(7)&sounddata&"000000" WHEN TwoUp,
+		sounddata(7)&sounddata(7)&sounddata(7)&sounddata&"00000" WHEN OneUp,
+		sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata&"0000" WHEN NoUp,
+		sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata&"000" WHEN OneDown,
+		sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata&"00" WHEN TwoDown,
+		sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata&"0" WHEN ThreeDown,
+		sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata WHEN FourDown,
+		prev_RL_DATA WHEN OTHERS;
 	
 
 		
