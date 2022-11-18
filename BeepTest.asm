771e1d8c81255c5f0e777ad36febef47809a6035
hyoshizaki <79033192+hyoshizaki@users.noreply.github.com>
2022-11-18 12:02:21 -0500

    Add files via upload

--- BeepTest.asm
+++ BeepTest.asm
@@ -3,94 +3,140 @@
 ; tone generator peripheral once per second.
 
 ORG 0
-
-; Get the switch values
 	IN Switches
 	JPOS SomeNote
-	
+	JNEG SomeNote
 	LOADI 0
-	STORE Temp
 	OUT Beep
-	CALL Delay
-	JUMP End
-
+	IN Beep
+	ADDI 0
+	OUT Hex0
+	JUMP 0
 SomeNote:
 	IN Switches
 	AND Bit0
-	JZERO NotC4
-	LOAD C4
-	STORE Temp
-	OUT Beep
-	CALL Delay
-	JUMP End
-NotC4:
+	JZERO NotC2
+	LOAD C2
+	STORE Note
+	JUMP FindOctave
+NotC2:
 	IN Switches
 	AND Bit1
-	JZERO NotD4
-	LOAD D4
-	STORE Temp
-	OUT Beep
-	CALL Delay
-	JUMP End
-NotD4:
+	JZERO NotD2
+	LOAD D2
+	STORE Note
+	JUMP FindOctave
+NotD2:
 	IN Switches
 	AND Bit2
-	JZERO NotE4
-	LOAD E4
-	STORE Temp
-	OUT Beep
-	CALL Delay
-	JUMP End
-NotE4:
+	JZERO NotE2
+	LOAD E2
+	STORE Note
+	JUMP FindOctave
+NotE2:
 	IN Switches
 	AND Bit3
-	JZERO NotF4
-	LOAD F4
-	STORE Temp
-	OUT Beep
-	CALL Delay
-	JUMP End
-NotF4:
+	JZERO NotF2
+	LOAD F2
+	STORE Note
+	JUMP FindOctave
+NotF2:
 	IN Switches
 	AND Bit4
-	JZERO NotG4
-	LOAD G4
-	STORE Temp
-	OUT Beep
-	CALL Delay
-	JUMP End
-NotG4:
+	JZERO NotG2
+	LOAD G2
+	STORE Note
+	JUMP FindOctave
+NotG2:
 	IN Switches
 	AND Bit5
-	JZERO NotA4
-	LOAD A4
-	STORE Temp
-	OUT Beep
-	CALL Delay
-	JUMP End
-NotA4:
+	JZERO NotA2
+	LOAD A2
+	STORE Note
+	JUMP FindOctave
+NotA2:
 	IN Switches
 	AND Bit6
 	JZERO 0
-	LOAD B4
-	STORE Temp
-	OUT Beep
-	CALL Delay
-	JUMP End
-
-End:
-	IN Beep
+	LOAD B2
 	STORE Note
-	LOAD Temp
+	JUMP FindOctave
+
+FindOctave:
+	IN Switches
+	AND Mask012
+	JZERO FirstOctave
+	ADDI -1
+	JZERO SecondOctave
+	ADDI -1
+	JZERO ThirdOctave
+	ADDI -1
+	JZERO FourthOctave
+	ADDI -1
+	JZERO FifthOctave
+	ADDI -1
+	JZERO SixthOctave
+	ADDI -1
+	JZERO SeventhOctave
+	ADDI -1
+	JZERO EigthOctave
+	JUMP 0
+
+FirstOctave:
+	LOAD Note
+	OUT Beep
+	JUMP End
+SecondOctave:
+	LOAD Note
+	SHIFT 1
+	STORE Note
+	OUT Beep
+	JUMP End
+ThirdOctave:
+	LOAD Note
+	SHIFT 2
+	STORE Note
+	OUT Beep
+	JUMP End
+FourthOctave:
+	LOAD Note
+	SHIFT 3
+	STORE Note
+	OUT Beep
+	JUMP End
+FifthOctave:
+	LOAD Note
+	SHIFT 4
+	STORE Note
+	OUT Beep
+	JUMP End
+SixthOctave:
+	LOAD Note
+	SHIFT 5
+	STORE Note
+	OUT Beep
+	JUMP End
+SeventhOctave:
+	LOAD Note
+	SHIFT 6
+	STORE Note
+	OUT Beep
+	JUMP End
+EigthOctave:
+	LOAD Note
+	SHIFT 7
+	STORE Note
+	OUT Beep
+
+	JUMP End
+End:
+	IN Beep	
 	SUB Note
-	OUT Hex0
-	Jump 0
-
-
-	
+	OUT Hex0 ;REMOVED RN FOR DEBUGGING
+	JUMP 0
 	
 
-	
+
 ; Subroutine to delay for 0.2 seconds.
 Delay:
 	OUT    Timer
@@ -106,25 +152,23 @@ LEDs:      EQU 001
 Timer:     EQU 002
 Hex0:      EQU 004
 Hex1:      EQU 005
-Temp:	   EQU 006
 Beep:      EQU &H40
-Note:      EQU 007
 
+C2:  	   DW 65
+D2:        DW 73
+E2:  	   DW 82
+F2:        DW 87
+G2:   	   DW 98
+A2:   	   DW 110
+B2:   	   DW 123
 
-C4:		   DW 65
-D4: 	   DW 73
-E4:		   DW 82
-F4:  	   DW 87
-G4:		   DW 98
-A4:		   DW 110
-B4:		   DW 123
+Note:	   DW 0
 
-
-Bit0:      DW &B0000000001
-Bit1:      DW &B0000000010
-Bit2:      DW &B0000000100
-Bit3:      DW &B0000001000
-Bit4:      DW &B0000010000
-Bit5:      DW &B0000100000
-Bit6:      DW &B0001000000
-AbsoluteValue:	DW &B0111111111
+Bit0:      DW &B0000001000
+Bit1:      DW &B0000010000
+Bit2:      DW &B0000100000
+Bit3:      DW &B0001000000
+Bit4:      DW &B0010000000
+Bit5:      DW &B0100000000
+Bit6:      DW &B1000000000
+Mask012:   DW &B111
--- TONE_GEN.vhd
+++ TONE_GEN.vhd
@@ -1,12 +1,6 @@
--- Simple DDS tone generator.
--- 5-bit tuning word
--- 9-bit phase register
 -- 256 x 8-bit ROM.
-
-
--- want: 13-bit phase register
--- 7-bit tuning word
---probably need to expand the ROM
+-- 16-bit phase register
+-- 13-bit tuning word
 
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
@@ -25,8 +19,8 @@ ENTITY TONE_GEN IS
 		CS         : IN  STD_LOGIC;
 		SAMPLE_CLK : IN  STD_LOGIC;
 		RESETN     : IN  STD_LOGIC;
-		L_DATA     : INOUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-		R_DATA     : INOUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+		L_DATA     : inOUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+		R_DATA     : inOUT STD_LOGIC_VECTOR(15 DOWNTO 0);
 		IO_WRITE	  : IN STD_LOGIC;
 		KEY2		  : IN STD_LOGIC; --Volume Down
 		KEY3		  : IN STD_LOGIC  --Volume Up
@@ -42,14 +36,10 @@ ARCHITECTURE gen OF TONE_GEN IS
 									TwoDown, ThreeDown, FourDown);
 	SIGNAL vol_state : VOL_STATE_TYPE;
 	
-	TYPE INTERVAL_STATE_TYPE IS (NoSound, Playing, TimeUp);
-	SIGNAL interval_state : INTERVAL_STATE_TYPE;
-	SIGNAL previousTuningWord : STD_LOGIC_VECTOR(12 downto 0);
-
-	SIGNAL phase_register : STD_LOGIC_VECTOR(15 DOWNTO 0); --changed, but may not compile
-	SIGNAL tuning_word    : STD_LOGIC_VECTOR(12 DOWNTO 0); --changed to make it 7-bit tuning word
+	SIGNAL phase_register : STD_LOGIC_VECTOR(15 DOWNTO 0); 
+	SIGNAL tuning_word    : STD_LOGIC_VECTOR(12 DOWNTO 0); --changed to make it 13-bit tuning word
+	SIGNAL prev_tuning_word    : STD_LOGIC_VECTOR(12 DOWNTO 0); --copy of tuning word
 	SIGNAL sounddata      : STD_LOGIC_VECTOR(7 DOWNTO 0);
-	SIGNAL KEY_PRESSED    : STD_LOGIC;
 	SIGNAL prev_RL_DATA   : STD_LOGIC_VECTOR(15 downto 0);
 	SIGNAL counter 		 : STD_LOGIC_VECTOR(31 DOWNTO 0);
 	
@@ -72,40 +62,35 @@ BEGIN
 	)
 	PORT MAP (
 		clock0 => NOT(SAMPLE_CLK),
-		-- In this design, one bit of the phase register is a fractional bit
-		-- will need to expand this part but will need to check how to...can i put in more bits into address_a?
+		-- In this design, 8 bits of the phase register are fractional bits
 		address_a => phase_register(15 downto 8),
 		q_a => sounddata -- output is amplitude
 	);
 	
+	--capability to read from the peripheral
 	CMD <=  CONV_STD_LOGIC_VECTOR(CONV_INTEGER(CONV_INTEGER("000" & tuning_word)*48000/65536),16) when
 			((CS = '1') AND (IO_WRITE = '0'))
 			ELSE "ZZZZZZZZZZZZZZZZ";
 	
-	KEY_PRESSED <= '1' WHEN (KEY2 = '1' OR KEY3 = '1') ELSE 
-						'0';
-	
-
-
-	-- 8-bit sound data is used as bits 12-5 of the 16-bit output.
-	-- This is to prevent the output from being too loud.
-	-- for volume control, shift left/right?
-	-- for splitting, make a internal signal
-		-- process to perform DDS
 	PROCESS(RESETN, SAMPLE_CLK) BEGIN
 		IF RESETN = '0' THEN
 			phase_register <= "0000000000000000";
 			counter <= x"00000000";
+			prev_tuning_word <= tuning_word; 
 		ELSIF RISING_EDGE(SAMPLE_CLK) THEN
-			if (counter = x"0EE6B280") OR (tuning_word = "0000000000000") then  -- if command is 0, return to 0 output.
+			if (counter = x"0003A980") OR (tuning_word = "0000000000000") then  -- if command is 0, return to 0 output.
 				phase_register <= "0000000000000000";
-				counter <= x"00000000";
 			ELSE
 				-- Increment the phase register by the tuning word. 
-				--**CHANGE HERE
 				phase_register <= phase_register + tuning_word;
 				counter <= counter + 1;
 			END IF;
+			
+			IF not(prev_tuning_word = tuning_word) then -- user changes key
+				counter <= x"00000000"; 
+				prev_tuning_word <= tuning_word; 
+			END IF; 
+				
 		END IF;
 	END PROCESS;
 
@@ -113,7 +98,6 @@ BEGIN
 	PROCESS(RESETN, CS) BEGIN
 		IF RESETN = '0' THEN
 			tuning_word <= "0000000000000";
-			previousTuningWord <= tuning_word;
 		ELSIF RISING_EDGE(CS) THEN
 			IF IO_WRITE = '1' THEN
 				tuning_word <= CONV_STD_LOGIC_VECTOR(CONV_INTEGER(CONV_INTEGER(CMD)*65536/48000), 13);
@@ -121,12 +105,13 @@ BEGIN
 		END IF;
 	END PROCESS;
 	
+	
 	--PROCESS statement to control volume
 	PROCESS(KEY2, KEY3, resetn, SAMPLE_CLK) BEGIN
 		if(resetn = '0') then
 			vol_state <= NoUp;
 		elsif(RISING_EDGE(SAMPLE_CLK)) THEN
-			IF ((KEY3 = '0') AND (KEY2 = '1')) THEN --KEY3 PRESSED and released so volume up
+			IF ((KEY3 = '0') AND (KEY2 = '1')) THEN --KEY3 PRESSED and released so request volume up
 				CASE vol_state IS
 					WHEN FourDown => vol_state <= WantThreeDown;
 					WHEN ThreeDown => vol_state <= WantTwoDown;
@@ -139,7 +124,7 @@ BEGIN
 					WHEN OTHERS => vol_state <= vol_state;
 				END Case;
 				prev_RL_DATA <= R_DATA;
-			elsif ((KEY2 = '0') AND (KEY3 = '1')) THEN --KEY2 pressed and released so volume down
+			elsif ((KEY2 = '0') AND (KEY3 = '1')) THEN --KEY2 pressed and released so request volume down
 				CASE vol_state is
 					WHEN FourUp => vol_state <= WantThreeUp;
 					WHEN ThreeUp => vol_state <= WantTwoUp;
@@ -152,7 +137,7 @@ BEGIN
 					WHEN OTHERS => vol_state <= vol_state;
 				END CASE;
 				prev_RL_DATA <= R_DATA;
-			elsif ((KEY2 = '1') AND (KEY3 = '1')) THEN
+			elsif ((KEY2 = '1') AND (KEY3 = '1')) THEN --both keys released so actually change the volume
 				CASE vol_state is
 					WHEN WantFourDown => vol_state <= FourDown;
 					WHEN WantThreeDown => vol_state <= ThreeDown;
@@ -191,7 +176,6 @@ BEGIN
 		sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata&"0" WHEN ThreeDown,
 		sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata(7)&sounddata WHEN FourDown,
 		prev_RL_DATA WHEN OTHERS;
-	
 
 		
 END gen;
